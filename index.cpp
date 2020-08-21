6f196b924b736967961ed8085912f12e71dc798a
Mehul Arora <mehul_arora@live.com>
2020-08-21 18:49:39 +0530

    All cards

---
+++ index.cpp
@@ -0,0 +1,503 @@
+#include <iostream> 
+
+using std::string;
+
+class Card {
+    public:
+        string color;
+        string texture;
+        string shape;
+        string quantity;
+};
+
+int main() {
+
+    Card one;
+    one.color = "red";
+    one.texture = "filled";
+    one.shape = "diamond";
+    one.quantity = "1";
+
+    Card two;
+    two.color = "red";
+    two.texture = "filled";
+    two.shape = "diamond";
+    two.quantity = "2";
+
+    Card three;
+    three.color = "red";
+    three.texture = "filled";
+    three.shape = "diamond";
+    three.quantity = "3";
+
+    Card four;
+    four.color = "green";
+    four.texture = "filled";
+    four.shape = "diamond";
+    four.quantity = "1";
+
+    Card five;
+    five.color = "green";
+    five.texture = "filled";
+    five.shape = "diamond";
+    five.quantity = "2";
+
+    Card six;
+    six.color = "green";
+    six.texture = "filled";
+    six.shape = "diamond";
+    six.quantity = "3";
+
+    Card seven;
+    seven.color = "purple";
+    seven.texture = "filled";
+    seven.shape = "diamond";
+    seven.quantity = "1";
+
+    Card eight;
+    eight.color = "purple";
+    eight.texture = "filled";
+    eight.shape = "diamond";
+    eight.quantity = "2";
+
+    Card nine;
+    nine.color = "purple";
+    nine.texture = "filled";
+    nine.shape = "diamond";
+    nine.quantity = "3";
+
+    Card ten;
+    ten.color = "red";
+    ten.texture = "empty";
+    ten.shape = "diamond";
+    ten.quantity = "1";
+
+    Card eleven;
+    eleven.color = "red";
+    eleven.texture = "empty";
+    eleven.shape = "diamond";
+    eleven.quantity = "2";
+
+    Card twelve;
+    twelve.color = "red";
+    twelve.texture = "empty";
+    twelve.shape = "diamond";
+    twelve.quantity = "3";
+
+    Card thirteen;
+    thirteen.color = "green";
+    thirteen.texture = "empty";
+    thirteen.shape = "diamond";
+    thirteen.quantity = "1";
+   
+    Card fourteen;
+    fourteen.color = "green";
+    fourteen.texture = "empty";
+    fourteen.shape = "diamond";
+    fourteen.quantity = "2";
+
+    Card fifteen;
+    fifteen.color = "green";
+    fifteen.texture = "empty";
+    fifteen.shape = "diamond";
+    six.quantity = "3";
+
+    Card sixteen;
+    sixteen.color = "purple";
+    sixteen.texture = "empty";
+    sixteen.shape = "diamond";
+    sixteen.quantity = "1";
+
+    Card seventeen;
+    seventeen.color = "purple";
+    seventeen.texture = "empty";
+    seventeen.shape = "diamond";
+    seventeen.quantity = "2";
+
+    Card eighteen;
+    eighteen.color = "purple";
+    eighteen.texture = "empty";
+    eighteen.shape = "diamond";
+    eighteen.quantity = "3";
+
+    Card nineteen;
+    nineteen.color = "red";
+    nineteen.texture = "shaded";
+    nineteen.shape = "diamond";
+    nineteen.quantity = "1";
+
+    Card twenty;
+    twenty.color = "red";
+    twenty.texture = "shaded";
+    twenty.shape = "diamond";
+    twenty.quantity = "2";
+
+    Card twentyone;
+    twentyone.color = "red";
+    twentyone.texture = "shaded";
+    twentyone.shape = "diamond";
+    twentyone.quantity = "3";
+
+    Card twentytwo;
+    twentytwo.color = "green";
+    twentytwo.texture = "shaded";
+    twentytwo.shape = "diamond";
+    twentytwo.quantity = "1";
+
+    Card twentythree;
+    twentythree.color = "green";
+    twentythree.texture = "shaded";
+    twentythree.shape = "diamond";
+    twentythree.quantity = "2";
+
+    Card twentyfour;
+    twentyfour.color = "green";
+    twentyfour.texture = "shaded";
+    twentyfour.shape = "diamond";
+    twentyfour.quantity = "3";
+
+    Card twentyfive;
+    twentyfive.color = "purple";
+    twentyfive.texture = "shaded";
+    twentyfive.shape = "diamond";
+    twentyfive.quantity = "1";
+
+    Card twentysix;
+    twentysix.color = "purple";
+    twentysix.texture = "shaded";
+    twentysix.shape = "diamond";
+    twentysix.quantity = "2";
+
+    Card twentyseven;
+    twentyseven.color = "purple";
+    twentyseven.texture = "shaded";
+    twentyseven.shape = "diamond";
+    twentyseven.quantity = "3";
+
+    Card twentyeight;
+    twentyeight.color = "red";
+    twentyeight.texture = "filled";
+    twentyeight.shape = "oval";
+    twentyeight.quantity = "1";
+
+    Card twentynine;
+    twentynine.color = "red";
+    twentynine.texture = "filled";
+    twentynine.shape = "oval";
+    twentynine.quantity = "2";
+
+    Card thirty;
+    thirty.color = "red";
+    thirty.texture = "filled";
+    thirty.shape = "oval";
+    thirty.quantity = "3";
+
+    Card thirtyone;
+    thirtyone.color = "green";
+    thirtyone.texture = "filled";
+    thirtyone.shape = "oval";
+    thirtyone.quantity = "1";
+
+    Card thirtytwo;
+    thirtytwo.color = "green";
+    thirtytwo.texture = "filled";
+    thirtytwo.shape = "oval";
+    thirtytwo.quantity = "2";
+
+    Card thirtythree;
+    thirtythree.color = "green";
+    thirtythree.texture = "filled";
+    thirtythree.shape = "oval";
+    thirtythree.quantity = "3";
+
+    Card thirtyfour;
+    thirtyfour.color = "purple";
+    thirtyfour.texture = "filled";
+    thirtyfour.shape = "oval";
+    thirtyfour.quantity = "1";
+
+    Card thirtyfive;
+    thirtyfive.color = "purple";
+    thirtyfive.texture = "filled";
+    thirtyfive.shape = "oval";
+    thirtyfive.quantity = "2";
+
+    Card thirtysix;
+    thirtysix.color = "purple";
+    thirtysix.texture = "filled";
+    thirtysix.shape = "oval";
+    thirtysix.quantity = "3";
+
+    Card thirtyseven;
+    thirtyseven.color = "red";
+    thirtyseven.texture = "empty";
+    thirtyseven.shape = "oval";
+    thirtyseven.quantity = "1";
+
+    Card thirtyeight;
+    thirtyeight.color = "red";
+    thirtyeight.texture = "empty";
+    thirtyeight.shape = "oval";
+    thirtyeight.quantity = "2";
+
+    Card thirtynine;
+    thirtynine.color = "red";
+    thirtynine.texture = "empty";
+    thirtynine.shape = "oval";
+    thirtynine.quantity = "3";
+
+    Card forty;
+    forty.color = "green";
+    forty.texture = "empty";
+    forty.shape = "oval";
+    forty.quantity = "1";
+    
+    Card fortyone;
+    fortyone.color = "green";
+    fortyone.texture = "empty";
+    fortyone.shape = "oval";
+    fortyone.quantity = "2";
+        
+    Card fortytwo;
+    fortytwo.color = "green";
+    fortytwo.texture = "empty";
+    fortytwo.shape = "oval";
+    fortytwo.quantity = "3";
+
+    Card fortythree;
+    fortythree.color = "purple";
+    fortythree.texture = "empty";
+    fortythree.shape = "oval";
+    fortythree.quantity = "1";
+        
+    Card fortyfour;
+    fortyfour.color = "purple";
+    fortyfour.texture = "empty";
+    fortyfour.shape = "oval";
+    fortyfour.quantity = "2";
+        
+    Card fortyfive;
+    fortyfive.color = "purple";
+    fortyfive.texture = "empty";
+    fortyfive.shape = "oval";
+    fortyfive.quantity = "3";
+        
+    Card fortysix;
+    fortysix.color = "red";
+    fortysix.texture = "empty";
+    fortysix.shape = "curvy";
+    fortysix.quantity = "1";
+        
+    Card fortyseven;
+    fortyseven.color = "red";
+    fortyseven.texture = "empty";
+    fortyseven.shape = "curvy";
+    fortyseven.quantity = "2";
+        
+    Card fortyeight;
+    fortyeight.color = "red";
+    fortyeight.texture = "empty";
+    fortyeight.shape = "curvy";
+    fortyeight.quantity = "3";
+        
+    Card fortynine;
+    fortynine.color = "green";
+    fortynine.texture = "empty";
+    fortynine.shape = "curvy";
+    fortynine.quantity = "1";
+            
+    Card fifty;
+    fifty.color = "green";
+    fifty.texture = "empty";
+    fifty.shape = "curvy";
+    fifty.quantity = "2";
+        
+    Card fiftyone;
+    fiftyone.color = "green";
+    fiftyone.texture = "empty";
+    fiftyone.shape = "curvy";
+    fiftyone.quantity = "3";
+        
+    Card fiftytwo;
+    fiftytwo.color = "purple";
+    fiftytwo.texture = "empty";
+    fiftytwo.shape = "curvy";
+    fiftytwo.quantity = "1";
+        
+    Card fiftythree;
+    fiftyone.color = "purple";
+    fiftyone.texture = "empty";
+    fiftyone.shape = "curvy";
+    fiftyone.quantity = "2";
+        
+    Card fiftyfour;
+    fiftyfour.color = "purple";
+    fiftyfour.texture = "empty";
+    fiftyfour.shape = "curvy";
+    fiftyfour.quantity = "3";
+        
+    Card fiftyfive;
+    fiftyfive.color = "red";
+    fiftyfive.texture = "shaded";
+    fiftyfive.shape = "diamond";
+    fiftyfive.quantity = "1";
+        
+    Card fiftysix;
+    fiftysix.color = "red";
+    fiftysix.texture = "shaded";
+    fiftysix.shape = "diamond";
+    fiftysix.quantity = "2";
+        
+    Card fiftyseven;
+    fiftyseven.color = "red";
+    fiftyseven.texture = "shaded";
+    fiftyseven.shape = "diamond";
+    fiftyseven.quantity = "3";
+        
+    Card fiftyeight;
+    fiftyeight.color = "green";
+    fiftyeight.texture = "shaded";
+    fiftyeight.shape = "diamond";
+    fiftyeight.quantity = "1";
+        
+    Card fiftynine;
+    fiftynine.color = "green";
+    fiftynine.texture = "shaded";
+    fiftynine.shape = "diamond";
+    fiftynine.quantity = "2";
+        
+    Card sixty;
+    sixty.color = "green";
+    sixty.texture = "shaded";
+    sixty.shape = "diamond";
+    sixty.quantity = "3";
+        
+    Card sixtyone;
+    sixtyone.color = "purple";
+    sixtyone.texture = "shaded";
+    sixtyone.shape = "diamond";
+    sixtyone.quantity = "1";
+            
+    Card sixtytwo;
+    sixtytwo.color = "purple";
+    sixtytwo.texture = "shaded";
+    sixtytwo.shape = "diamond";
+    sixtytwo.quantity = "2";
+
+    Card sixtythree;
+    sixtythree.color = "purple";
+    sixtythree.texture = "shaded";
+    sixtythree.shape = "diamond";
+    sixtythree.quantity = "3";
+
+    Card sixtyfour;
+    sixtyfour.color = "red";
+    sixtyfour.texture = "shaded";
+    sixtyfour.shape = "oval";
+    sixtyfour.quantity = "1";
+
+    Card sixtyfive;
+    sixtyfive.color = "red";
+    sixtyfive.texture = "shaded";
+    sixtyfive.shape = "oval";
+    sixtyfive.quantity = "2";
+
+    Card sixtysix;
+    sixtysix.color = "red";
+    sixtysix.texture = "shaded";
+    sixtysix.shape = "oval";
+    sixtysix.quantity = "3";
+
+    Card sixtyseven;
+    sixtyseven.color = "green";
+    sixtyseven.texture = "shaded";
+    sixtyseven.shape = "oval";
+    sixtyseven.quantity = "1";
+
+    Card sixtyeight;
+    sixtyeight.color = "green";
+    sixtyeight.texture = "shaded";
+    sixtyeight.shape = "oval";
+    sixtyeight.quantity = "2";
+
+    Card sixtynine;
+    sixtynine.color = "green";
+    sixtynine.texture = "shaded";
+    sixtynine.shape = "oval";
+    sixtynine.quantity = "3";
+
+    Card seventy;
+    seventy.color = "purple";
+    seventy.texture = "shaded";
+    seventy.shape = "oval";
+    seventy.quantity = "1";
+
+    Card seventyone;
+    seventyone.color = "purple";
+    seventyone.texture = "shaded";
+    seventyone.shape = "oval";
+    seventyone.quantity = "2";
+
+    Card seventytwo;
+    seventytwo.color = "purple";
+    seventytwo.texture = "shaded";
+    seventytwo.shape = "oval";
+    seventytwo.quantity = "3";
+
+    Card seventythree;
+    seventythree.color = "red";
+    seventythree.texture = "shaded";
+    seventythree.shape = "curvy";
+    seventythree.quantity = "1";
+
+    Card seventyfour;
+    seventyfour.color = "red";
+    seventyfour.texture = "shaded";
+    seventyfour.shape = "curvy";
+    seventyfour.quantity = "2";
+
+    Card seventyfive;
+    seventyfive.color = "red";
+    seventyfive.texture = "shaded";
+    seventyfive.shape = "curvy";
+    seventyfive.quantity = "3";
+
+    Card seventysix;
+    seventysix.color = "green";
+    seventysix.texture = "shaded";
+    seventysix.shape = "curvy";
+    seventysix.quantity = "1";
+
+    Card seventyseven;
+    seventyseven.color = "green";
+    seventyseven.texture = "shaded";
+    seventyseven.shape = "curvy";
+    seventyseven.quantity = "2";
+
+    Card seventyeight;
+    seventyeight.color = "green";
+    seventyeight.texture = "shaded";
+    seventyeight.shape = "curvy";
+    seventyeight.quantity = "3";
+
+    Card seventynine;
+    seventynine.color = "purple";
+    seventynine.texture = "shaded";
+    seventynine.shape = "curvy";
+    seventynine.quantity = "1";
+
+    Card eighty;
+    eighty.color = "purple";
+    eighty.texture = "shaded";
+    eighty.shape = "curvy";
+    eighty.quantity = "2";
+
+    Card eightyone;
+    eightyone.color = "purple";
+    eightyone.texture = "shaded";
+    eightyone.shape = "curvy";
+    eightyone.quantity = "3";
+
+
+
+}
